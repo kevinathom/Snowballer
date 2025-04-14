dbedd9f727ec91f5bf2fb07d0581516db168898d
Kevin A Thomas <44673990+kevinathom@users.noreply.github.com>
2025-04-14 16:52:06 -0400

    Update user_variables.py
    Added error-handling. Canceling windows or setting invalid values will either set variables to safe defaults or exit with a message. Note: This does not check that the selected seed file has valid content.

--- code/user_variables.py
+++ code/user_variables.py
@@ -11,6 +11,7 @@ from tkinter import simpledialog
 from tkinter import filedialog
 from tkinter import messagebox
 import re
+import sys
 
 # Define functions
 ## Show text entry window
@@ -147,26 +148,53 @@ def show_completion_message(your_title="Process Complete", your_message="The pro
 # Prompt user for variables
 ## Set user email
 my_email = get_email_input(your_title="Email", your_message="Enter your email address for the OpenAlex API.")
+if not ('my_email' in locals() or 'my_email' in globals()):
+    my_email = ""
+if my_email == None:
+    my_email = ""
 
 ## Set seed work entity ID(s)
 seed_file = open_file_dialog(your_title = "Select a CSV file containing the work entity ID(s) to use as a starting point.")
+if not ('seed_file' in locals() or 'seed_file' in globals()):
+    show_completion_message(your_title="Process Cancelled", your_message="Did not receive a work entity ID file.")
+    sys.exit(1) # Terminate with code 1, no valid seed file
+if seed_file == None or seed_file == '':
+    show_completion_message(your_title="Process Cancelled", your_message="Did not receive a work entity ID file.")
+    sys.exit(1) # Terminate with code 1, no valid seed file
 
 ## Set degrees of separation
 cited_by = get_integer_input(your_title="'Cited By' Degrees", your_message="This tool will identify works cited by the work(s) you specify.\n" + \
                             "How many degrees of separation do you want to retrieve?\n" + \
                             "(e.g.: Enter 2 to find works cited by the work(s) you provide\n" + \
                             "plus works cited by those works.)", initial_value="1")
+if not ('cited_by' in locals() or 'cited_by' in globals()):
+    cited_by = 0
+if cited_by == None or cited_by == '':
+    cited_by = 0
+    
 cites = get_integer_input(your_title="'Cites' Degrees", your_message="This tool will identify works that cite the work(s) you specify.\n" + \
                             "How many degrees of separation do you want to retrieve?\n" + \
                             "(e.g.: Enter 2 to find works that cite the work(s) you provide\n" + \
                             "plus works that cite those works.)", initial_value="1")
+if not ('cites' in locals() or 'citeds' in globals()):
+    cites = 0
+if cites == None or cites == '':
+    cites = 0
 
 ## Set directories
 data_dir = open_directory_dialog(your_title = "Select a directory to hold working and results files.")
+if not ('data_dir' in locals() or 'data_dir' in globals()):
+    show_completion_message(your_title="Process Cancelled", your_message="Did not receive a directory for data.")
+    sys.exit(2) # Terminate with code 2, no valid data directory
+if data_dir == None or data_dir == '':
+    show_completion_message(your_title="Process Cancelled", your_message="Did not receive a directory for data.")
+    sys.exit(2) # Terminate with code 2, no valid data directory
+
 #code_dir = open_directory_dialog(your_title = "Select the directory that holds your copy of Snowballer's code files.")
 
 ## Set fields to request for works
 fields_to_return = ['id', 'doi', 'title', 'publication_year', 'language', 'type', 'is_retracted']
+
 
 # Clean up
 gc.collect()
