3689a180bbc648783d9f67906c61ed2c39d88e52
kevinathom <44673990+kevinathom@users.noreply.github.com>
2026-08-03 13:31:47 -0400

    Update read_seeds.py
    Convert the ID cleaning process into a function for easier reuse. Update the author name

--- code/read_seeds.py
+++ code/read_seeds.py
@@ -1,15 +1,32 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Mar  5 17:50:02 2025
-@author: kevinatpenn
+@author: kevinathom
 Purpose: Ingest seed work IDs
 """
+
+# Define functions
+def clean_work_ids(ids_list):
+  """
+  Clean, standardize, and deduplicate OpenAlex work IDs
+  
+  Args:
+    ids (list): Work IDs, potentially mixed with other content
+  
+  Returns:
+    list: Clean work IDs
+  """
+  # Remove web address prefix that appears in OpenAlex extracts
+  ids_list = [i.replace('https://openalex.org/', '') for i in ids_list]
+  # Keep only items formatted like work IDs
+  ids_list = [x for x in ids_list if re.match(r'[Ww]\d+', x)]
+  # Remove duplicates
+  ids_list = list(set(ids_list))
+  
+  return ids_list
 
 # Read first column of seed_file
 seed_ids = pd.read_csv(seed_file, sep=',', header=None, usecols=[0])
 
-# Clean work ID values
-seed_ids = seed_ids[0].tolist()
-seed_ids = [s.replace('https://openalex.org/', '') for s in seed_ids] # Remove web address
-seed_ids = [x for x in seed_ids if re.match(r'[Ww]\d+', x)] # Filter for work ID format
-seed_ids = list(set(seed_ids)) # Remove duplicates
+# Clean and standardize work ID values
+seed_ids = clean_work_ids(seed_ids[0].tolist())
