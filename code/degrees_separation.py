7a31fc47c7524f4b084fb2ff0a29f1a03240a83c
kevinathom <44673990+kevinathom@users.noreply.github.com>
2026-08-04 18:44:15 -0400

    Update degrees_separation.py
    Converted script to handle a dictionary as input and return the same result as before. Has errors noted as comments.

--- code/degrees_separation.py
+++ code/degrees_separation.py
@@ -2,28 +2,29 @@
 """
 Created on Wed Mar  5 11:11:53 2025
 @author: kevinathom
-Purpose: Generate an object that encodes all degrees of 
-    separation to retrieve (relative to seed works)
+Purpose: Create lists of direction and degree of separation
+  for compatibility with legacy get_works.py process
 """
-
-#!!!To do: Convert the result to a dictionary for easier handling per direction/degree
 
 # Initialize objects
 cite_degrees = []
-deg = 1
 
-# Create vectors for get_works
-while cited_by > 0 or cites > 0:
-    # Check for 0 or more degrees
-    cited_by_deg = min(cited_by, 1)
-    cites_deg = min(cites, 1)
-    # Create a vector for the current degree of separation
-    cite_degrees.append(['cited_by'] * cited_by_deg + ['cites'] * cites_deg)
-    # Increment/Decrement counts
-    deg += 1
-    cited_by -= cited_by_deg
-    cites -= cites_deg
+# Create vectors
+#try this?: dirdegs_dict.get('cited_by') is not None
+#might have an issue with the pop process too
+while (('cited_by' in dirdegs_dict and dirdegs_dict['cited_by'] is not None)
+       or ('cites' in dirdegs_dict and dirdegs_dict['cites'] is not None)):
+  
+  cb_multiplier = 'cited_by' in dirdegs_dict or dirdegs_dict['cited_by'] is not None
+  if cb_multiplier:
+    dirdegs_dict.pop('cited_by')
+  
+  c_multiplier = 'cites' in dirdegs_dict or dirdegs_dict['cites'] is not None
+  if c_multiplier:
+    dirdegs_dict.pop('cites')
+    
+  # Create a vector for the current degree of separation
+  cite_degrees.append(['cited_by'] * cb_multiplier + ['cites'] * c_multiplier)
 
-# Clean up temporary objects
-#del cited_by, cited_by_deg, cites, cites_deg, deg
+# Clean up
 gc.collect()
