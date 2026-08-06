c54c2793ee24a8611926536d37cc87842e1e489f
kevinathom <44673990+kevinathom@users.noreply.github.com>
2026-08-06 14:21:29 -0400

    Update dedup_works.py
    Create functions to perform the explicit tasks in this script

--- code/dedup_works.py
+++ code/dedup_works.py
@@ -16,16 +16,51 @@ def list_files(directory, file_extension = ''):
   
   Args:
     directory (str): Directory to search for files
-    file_extension (str): 
+    file_extension (str): Any string to match at the end of a file
   
+  Returns:
+    list: File names as strings
   """
   files = [f for f in os.listdir(directory) if f.lower().endswith(file_extension)]
   return files
 
-# List TXT files in data directory
+def join_file_content(files_to_join, input_directory=os.getcwd(), to_join_delimiter='|', file_to_output='joined.csv', output_directory=os.getcwd(), output_delimiter=',', clean_up=False):
+  """
+  Joins delimited files and deduplicates rows.
+  
+  Args:
+    files_to_join (list): Strings containing the names of each file to join
+    input_directory (str): File path that holds each file in files_to_join
+    to_join_delimiter (str): Separater to use in delimited input files
+    file_to_output (str): Name, including file extension, to give the joined output file
+    output_directory (str): File path to hold the joined output file
+    output_delimiter (str): Separater to use in delimited output file
+    
+  Returns:
+    None, but it outputs a delimited file
+  """
+  if files_to_join:
+    # Load and de-duplicate first file
+    file = 0
+    content = pd.read_csv(os.path.join(input_directory, files_to_join[file]), sep=to_join_delimiter)
+    
+    # Load and de-duplicate remaining files
+    while file < len(files_to_join) - 1:
+      file += 1
+      next_df = pd.read_csv(os.path.join(input_directory, files_to_join[file]), sep=to_join_delimiter)
+      content = pd.concat([content, next_df], ignore_index=True).drop_duplicates()
+    
+    # Save results to file
+    works.to_csv(os.path.join(output_directory, file_to_output), sep=output_delimiter, index=False)
+  
+  if clean_up:
+    for file in files_to_join:
+      os.remove(os.path.join(input_directory, file))
+
+# List TXT files in data directory (legacy)
 fls = list_files(directory=working_dir, file_extension='.txt')
 
-# De-duplicate results
+# De-duplicate results (legacy)
 if fls:
     # Load and de-duplicate first file
     fl = 0
@@ -41,7 +76,7 @@ if fls:
     # Save results (to consolidated file)
     works.to_csv(os.path.join(working_dir, f'works_snowball_{direction}_{degree}.csv'), sep=',', index=False)
 
-# Clean up temporary directory
+# Clean up temporary directory (legacy)
 for file in fls:
     os.remove(os.path.join(working_dir, file))
 
