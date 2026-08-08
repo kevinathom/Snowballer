53a2ad6a6114f2f48c04c9a2eba2413a2d29aa1b
kevinathom <44673990+kevinathom@users.noreply.github.com>
2026-08-07 21:57:51 -0400

    Update dedup_works.py
    Remove manual code so the script provides only functions

--- code/dedup_works.py
+++ code/dedup_works.py
@@ -5,10 +5,6 @@ Created on Wed Mar  5 11:19:43 2025
 Purpose: Remove duplicate works and consolidate files
 """
 
-# List unique direction-degree values
-#import re
-#dirdegs = set([re.findall(r'W\d+_(cite[a-z_]+_\d+)\.txt', f) for f in fls]) #not a flat list
-
 def list_files(directory, file_extension = ''):
   """
   Lists files in a given directory. Limits results to files that match a file
@@ -56,28 +52,5 @@ def join_file_content(files_to_join, input_directory=os.getcwd(), to_join_delimi
   if clean_up:
     for file in files_to_join:
       os.remove(os.path.join(input_directory, file))
-
-# List TXT files in data directory (legacy)
-fls = list_files(directory=working_dir, file_extension='.txt')
-
-# De-duplicate results (legacy)
-if fls:
-    # Load and de-duplicate first file
-    fl = 0
-    works = pd.read_csv(os.path.join(working_dir, fls[fl]), sep='|')
-    works = works.drop_duplicates()
-    
-    # Handle remaining files
-    while fl < len(fls) - 1:
-        fl += 1
-        next_df = pd.read_csv(os.path.join(working_dir, fls[fl]), sep='|')
-        works = pd.concat([works, next_df], ignore_index=True).drop_duplicates()
-    
-    # Save results (to consolidated file)
-    works.to_csv(os.path.join(working_dir, f'works_snowball_{direction}_{degree}.csv'), sep=',', index=False)
-
-# Clean up temporary directory (legacy)
-for file in fls:
-    os.remove(os.path.join(working_dir, file))
 
 gc.collect()
